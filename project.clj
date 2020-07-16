d52a9b7b6f56e9635d3aed1c8d6575f88650805e
CI/CD <ci@pinkgorilla.org>
2020-07-16 18:42:31 +0000

    Begin 0.0.17-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/webly "0.0.16"
+(defproject org.pinkgorilla/webly "0.0.17-SNAPSHOT"
   :description "web (server / reagent) helper library."
   :url "https://github.com/pink-gorilla/webly"
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
