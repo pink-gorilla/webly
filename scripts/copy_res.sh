fed8c0b2c359d4198124ca57b033904f81583e81
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-07-18 17:58:15 +0200

    font awesome added

--- scripts/copy_res.sh
+++ scripts/copy_res.sh
@@ -1,4 +1,10 @@
-
+#!/bin/bash
 
 mkdir -p target/node_modules/public/tailwindcss/dist
 cp node_modules/tailwindcss/dist/*.*  target/node_modules/public/tailwindcss/dist
+
+mkdir -p target/node_modules/public/@fortawesome/fontawesome-free/css
+cp node_modules/@fortawesome/fontawesome-free/css/*.*  target/node_modules/public/@fortawesome/fontawesome-free/css
+
+mkdir -p target/node_modules/public/@fortawesome/fontawesome-free/webfonts
+cp node_modules/@fortawesome/fontawesome-free/webfonts/*  target/node_modules/public/@fortawesome/fontawesome-free/webfonts
--- src/webly/deps.cljs
+++ src/webly/deps.cljs
@@ -1,6 +1,8 @@
 {:npm-deps
  {"create-react-class" "^15.6.3"
+  "@fortawesome/fontawesome-free" "^5.13.0"  ; font awesome
   "react" "^16.13.0"
   "react-dom" "^16.13.0"
   "react-dom-factories" "^1.0.2"
-  "tailwindcss" "1.5.1"}}
+  "tailwindcss" "1.5.1" ; tailwind
+  }}
