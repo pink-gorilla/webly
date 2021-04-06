24f1f839cc4998c6b9a95e7128ca802afe37f4c2
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-04-06 00:06:52 -0500

    popper-flow deps

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,7 +2,7 @@
  {"@fortawesome/fontawesome-free" "^5.13.0"  ; font awesome
   "create-react-class" "^15.6.3"
   "marked" "^0.7.0" ; markdown -> html renderer
-  "oauth2-popup-flow" "^1.1.0"
+
   "react" "^16.13.0"
   "react-dom" "^16.13.0"
   "react-dom-factories" "^1.0.2"
@@ -11,4 +11,17 @@
   ; javascript unit tests 
   "karma" "^4.4.1"
   "karma-chrome-launcher" "^3.1.0"
-  "karma-cljs-test" "^0.1.0"}}
+  "karma-cljs-test" "^0.1.0"
+
+  ; oauth2-popup-flow (with dev dependencies)
+  "oauth2-popup-flow" "^1.1.0"
+  "@babel/preset-env" "^7.9.0"
+  "@babel/preset-typescript" "^7.9.0"
+  "@rollup/plugin-typescript" "^4.0.0"
+  "@types/jest" "^26.0.3"
+  "jest" "^26.1.0"
+  "jsdom" "^16.2.2"
+  "rollup" "^2.3.3"
+  "tslib" "^2.0.0"
+  "typedoc" "^0.17.4"
+  "typescript" "^3.8.3"}}
