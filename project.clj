6c6a2951ea54cd4a0f85f272be56d271c8c85e13
CI/CD <ci@pinkgorilla.org>
2021-04-14 01:03:24 +0000

    Release 0.1.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/webly "0.1.17-SNAPSHOT"
+(defproject org.pinkgorilla/webly "0.1.17"
   :description "web (server / reagent) helper library."
   :url "https://github.com/pink-gorilla/webly"
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
