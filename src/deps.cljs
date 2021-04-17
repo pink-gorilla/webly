9206b42b7565429bc0bdc1beeb182eb0c2480c97
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-04-17 15:20:11 -0500

    missig google font deps

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,17 +1,7 @@
 {:npm-deps
- {; font awesome
+ {; fonts
   "@fortawesome/fontawesome-free" "^5.14.0"
-
-   ; markdown -> html renderer
-  "marked" "^2.0.3"
-  ;  "highlight.js": "9.18.1",
-  ;     "react-highlight.js": "1.0.7",
-
-  ; react
-  "create-react-class" "^15.7.0"
-  "react" "^16.13.0"  ; reagent needs this version
-  "react-dom" "^16.13.0"  ; reagent needs this version
-  "react-dom-factories" "^1.0.2"
+  "get-google-fonts" "^1.2.2"
 
 ; tailwind
   "tailwindcss" "2.1.1"
@@ -21,5 +11,17 @@
   "karma" "^6.3.2"
   "karma-chrome-launcher" "^3.1.0"
   "karma-cljs-test" "^0.1.0"
+
+    ; react
+  "create-react-class" "^15.7.0"
+  "react" "^16.13.0"  ; reagent needs this version
+  "react-dom" "^16.13.0"  ; reagent needs this version
+  "react-dom-factories" "^1.0.2"
+
+  ; markdown -> html renderer
+  "marked" "^2.0.3"
+  ; "highlight.js": "9.18.1",
+  ; "react-highlight.js": "1.0.7",
+
   ;
   }}
