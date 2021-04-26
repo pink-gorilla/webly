53111cbaa4c1f920b56d2d5dd6523cfdf578e3ce
CI/CD <ci@pinkgorilla.org>
2021-04-26 07:43:21 +0000

    Begin 0.2.14-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/webly "0.2.13"
+(defproject org.pinkgorilla/webly "0.2.14-SNAPSHOT"
   :description "web (server / reagent) helper library."
   :url "https://github.com/pink-gorilla/webly"
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
