724d33eb0c47e5215269255160c31e540b49535f
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-04-30 17:20:06 -0500

    ci fixed

--- src/deps.cljs
+++ src/deps.cljs
@@ -6,7 +6,7 @@
             "tailwindcss" "2.1.2"
             "autoprefixer" "^10.0.2" ; peer dependency of tailwind. actually needed?
             "postcss" "^8.1.13"  ; peer dependency of tailwind
-            
+
             ; javascript unit tests 
             "karma" "^6.3.2"
             "karma-chrome-launcher" "^3.1.0"
