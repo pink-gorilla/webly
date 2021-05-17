46f40e8da8b855fff830dc8aa6e558168e43e425
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-16 22:33:31 -0500

    less logging

--- src/webly/ws/handler.clj
+++ src/webly/ws/handler.clj
@@ -1,6 +1,6 @@
 (ns webly.ws.handler
   (:require
-   [taoensso.timbre :as log :refer [debug info infof]]
+   [taoensso.timbre :as log :refer [debug debugf info infof]]
    [cheshire.core :as json]
    [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
    [webly.web.handler :refer [add-ring-handler]]
@@ -14,10 +14,7 @@
   ;(:anti-forgery-token ring-req)] 
   (force *anti-forgery-token*))
 
-
-
 ; WEBSOCKET
-
 
 (defn ws-token-handler-raw [req]
   (let [token {:csrf-token (get-csrf-token)}]
@@ -26,13 +23,14 @@
      :body (json/generate-string token)})) ; json must stay! sente has issues if middleware converts this
 
 (defn ws-handshake-handler [conn req]
-  (infof "ws/chsk GET: %s" req)
+  (debugf "ws/chsk GET: %s" req)
   (let [{:keys [ring-ajax-get-or-ws-handshake]} conn
         client-id  (get-in req [:params :client-id]) ; check if sente requirements are met
         uid (get-sente-session-uid req)
         res (ring-ajax-get-or-ws-handshake req)]
-    (infof "ws-chsk client-id: %s uid: %s sente" client-id uid)
-    (info "ws csrf: " (get-in req [:session :ring.middleware.anti-forgery/anti-forgery-token]))
+    (infof "ws-chsk client-id: %s uid: %s csrf: %s"
+           client-id uid
+           (get-in req [:session :ring.middleware.anti-forgery/anti-forgery-token]))
     (debug res)
     res))
 
