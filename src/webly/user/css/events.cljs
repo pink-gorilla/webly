3374833122e65336cb5c622e15e5df90b7366206
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-07-03 00:03:18 -0500

    css/init bugfix

--- src/webly/user/css/events.cljs
+++ src/webly/user/css/events.cljs
@@ -2,23 +2,18 @@
   (:require
    [taoensso.timbre :refer-macros [info error]]
    [clojure.string :as str]
-   [re-frame.core :refer [reg-event-db]]))
+   [re-frame.core :as rf]))
 
-(reg-event-db
+(rf/reg-event-db
  :css/init
  (fn [db [_]]
    (let [theme (get-in db [:config :webly :theme])
-         {:keys [available current]
-          :or {available {}
-               current {}}}
-         (or theme {})
-         theme-safe {:available available
-                     :current current}]
-     (info "css init: " theme-safe)
-     (assoc db :theme theme-safe)
+         {:keys [available current]} (or theme {})]
+     (info "css init: " theme)
+     (rf/dispatch [:css/add-components available current])
      db)))
 
-(reg-event-db
+(rf/reg-event-db
  :css/add-components
  (fn [db [_ components components-default-config]]
    (info "css add-component: " components-default-config)
@@ -31,7 +26,7 @@
                 :current (merge current components-default-config)}]
      (assoc db :theme theme))))
 
-(reg-event-db
+(rf/reg-event-db
  :css/set-theme-component
  (fn [db [_ component theme]]
    (assoc-in db [:theme :current component] theme)))
