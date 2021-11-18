9e0c68f6c47aca611c1b465419187abce9519a23
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-18 16:25:11 +0100

    oauth/make-requests

--- profiles/demo-oauth2/src/demo.clj
+++ profiles/demo-oauth2/src/demo.clj
@@ -1,30 +1,41 @@
 (ns demo
   (:require
+   [clojure.pprint :refer [print-table]]
+   [taoensso.timbre :as timbre :refer [info error]]
    [modular.config :as config]
    [modular.oauth2.store :refer [load-token]]
    [modular.oauth2.request :refer [get-endpoint get-request]]))
 
-(config/set!
- :oauth2
- {:token-path "../../.webly/tokenstore/"})
+(defn make-web-requests [& _]
 
-(load-token :google)
-(get-endpoint :google/search)
-(get-request :google/search)
-(get-request :google/search {:q "clojure"
-                             :num 10
+  (config/set!
+   :oauth2
+   {:token-path "../../.webly/tokenstore/"})
+
+  ; github
+  (load-token :github)
+  (get-endpoint :github/userinfo)
+
+  (->> (get-request :github/userinfo)
+       (info "github/userinfo: "))
+
+  (info "github/search-repo ")
+  (->> (get-request :github/search-repo {:q "user:pink-gorilla"})
+       :items
+       ;first
+       (map #(select-keys % [:name :forks_count :watchers_count :open_issues_count]))
+       (print-table))
+
+  (load-token :google)
+  (get-endpoint :google/search)
+  (get-request :google/userinfo)
+  #_(get-request :google/search {:q "clojure"
+                                 :num 10
                              ;:cx 4
-                             })
+                                 })
 
-(load-token :github)
-(get-endpoint :github/userinfo)
-(get-request :github/userinfo)
-(get-request :github/search-repo {:q "user:pink-gorilla"})
+  (load-token :xero)
 
-(load-token :xero)
-
-
-
-
-
+;  
+  )
 
