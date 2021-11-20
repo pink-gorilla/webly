c6ebd011c27785b3a1e2c544a27721a6bd15dc4d
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-20 23:06:47 +0100

    ci

--- script/deps.clj
+++ script/deps.clj
@@ -1,12 +1,18 @@
 (ns deps
   (:require
    [clojure.edn :as edn]
+   [clojure.pprint :as pp]
    ;[fipp.clojure]
    ))
 
 ;(defn pr-str-fipp [config]
 ;  (with-out-str
 ;    (fipp.clojure/pprint config {:width 40})))
+
+(defn pr-str-clj [config]
+  (binding [pp/*print-right-margin* 80]
+    (with-out-str
+    (pp/pprint config))))
 
 (defn load-deps-config-path [edn-path key-path]
   (-> (slurp (str edn-path "/deps.edn"))
@@ -18,11 +24,13 @@
         main-deps (:deps main-edn)
         others-deps-list (map #(load-deps-config-path % [:deps]) others)
         other-deps (apply concat others-deps-list)
-        all-deps (into [] (concat main-deps other-deps))
+        all-deps (concat main-deps other-deps)
+        all-deps (remove #(-> % second :local/root) all-deps)
+        all-deps (into {} all-deps)
         merged-edn (assoc main-edn :deps all-deps)]
     (println "merged deps: " all-deps)
     (->> merged-edn
-         (pr-str)
+         (pr-str-clj)
          (spit to))))
 
 
