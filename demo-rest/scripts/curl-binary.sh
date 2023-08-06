75fd3497a19758e9a240b27dbb975dd7ec089354
awb99 <awb99@gmail.com>
2023-08-05 20:37:48 -0500

    rest and permission moved to separate projects

--- demo-rest/scripts/curl-binary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl  http://localhost:8000/api/bindata \
-     -v \
-     -O /tmp/blog.bin
-
-#      -H 'Accept: application/transit+json' \
--- demo-rest/scripts/curl-wp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl -X GET https://www.crbclean.com/wp-json/wp/v2/product
-
