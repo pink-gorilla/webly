c99e87c0f2557630bf049ce8829d60ad21bda4ac
awb99 <awb99@gmail.com>
2024-03-11 19:18:38 -0500

    resource handler simplified

--- webly/src/webly/spa/handler/routes.clj
+++ webly/src/webly/spa/handler/routes.clj
@@ -1,12 +1,27 @@
 (ns webly.spa.handler.routes
   (:require
-   [webly.spa.handler.resources :refer [resource-handler file-handler-nodejs file-handler-bundel file-handler-code]]
-   [webly.spa.handler.routes-resolve :refer [resolve-handler]]
-   ))
+   [modular.webserver.handler.files :refer [->ResourcesMaybe ->FilesMaybe]]
+   [webly.spa.handler.routes-resolve :refer [resolve-handler]]))
 
 (defn make-routes-frontend [user-routes-app]
-  ["/" user-routes-app ;(merge webly-routes-app user-routes-app)
-  ])
+  ["/" user-routes-app])
+
+;; from: 
+;; https://github.com/juxt/bidi/blob/master/README.md
+;; 
+;; The Resources and ResourcesMaybe record can be used on the right-hand 
+;; side of a route. It serves resources from the classpath. After the 
+;; pattern is matched, the remaining part of the path is added to the given prefix.
+;; ["/resources" (->ResourcesMaybe {:prefix "public/"})
+;; There is an important difference between Resources and ResourcesMaybe. 
+;; Resources will return a 404 response if the resource cannot be found, while
+;;  ResourcesMaybe will return nil, allowing subsequent routes to be tried.
+
+(def resource-handler 
+  {#{"r" "bundel"} (->FilesMaybe {:dir "target/webly/public"})
+   #{"r" "node"}   (->FilesMaybe {:dir "node_modules"})
+   #{"r" "jarres"} (->ResourcesMaybe {:prefix "public"})
+   "code/"  (->ResourcesMaybe {:prefix ""})})
 
 
 (defn make-routes-backend [user-routes-api config-route websocket-routes]
@@ -15,10 +30,7 @@
         ]
     ["/" {"api/"    api-routes
           ; ""       app-routes
-          #{"r" "bundel"} file-handler-bundel ;["r"]  ; first from file, thereafter from resource.
-          #{"r" "node"} file-handler-nodejs
-          #{"r" "jarres"} resource-handler  ;"r"
-          "code/" file-handler-code
+          "" resource-handler
         ;[true      :webly/not-found]  ; not working as we need to process frontend routes also
           }]))
 
