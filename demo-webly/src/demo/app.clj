534b7f6526fa8c6b444f489fe9f4d6101c567a53
awb99 <awb99@gmail.com>
2024-04-03 12:12:53 -0500

    oauth2 removed (has its own demo)

--- demo-webly/src/demo/app.clj
+++
@@ -1,8 +0,0 @@
-(ns demo.app
-  (:require
-
-    ; todo: remove here, once oauth2 works with goldly.  
-   [modular.oauth2.handler] ; side-effects
-   [modular.oauth2] ; side-effects
-    ;[modular.oauth2.middleware :refer [print-oauth2-config]]
-   ))
