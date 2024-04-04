14104519fabe3aa3c3efeb1403561f105a968050
awb99 <awb99@gmail.com>
2024-04-03 20:21:33 -0500

    webly.spa.resolve

---
+++ spa/src/webly/spa/resolve.cljs
@@ -0,0 +1,14 @@
+(ns webly.spa.resolve
+  (:require 
+   [webly.module.build :refer [webly-resolve]]))
+
+
+(def resolver-a (atom webly-resolve))   
+
+
+(defn set-resolver! [resolver-fn]
+  (reset! resolver-a resolver-fn))
+
+(defn get-resolver []
+  @resolver-a)
+
