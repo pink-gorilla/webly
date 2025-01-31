f18d34403a2179a898b7b5045cf7eeca12346f32
awb99 <awb99@gmail.com>
2025-01-31 12:37:07 -0500

    config via promise

--- spa/src/webly/spa/config/subscription.cljs
+++
@@ -1,8 +0,0 @@
-(ns webly.spa.config.subscription
-  (:require
-   [re-frame.core :refer [reg-sub]]))
-
-(reg-sub
- :webly/config
- (fn [db _]
-   (get-in db [:config])))
--- spa/src/webly/spa/views.cljs
+++ spa/src/webly/spa/views.cljs
@@ -1,25 +1,16 @@
 (ns webly.spa.views
   (:require
-   [re-frame.core :refer [subscribe]]
    [frontend.css.view :refer [load-css]]
    [frontend.notification :refer [notification-container]]
    [frontend.dialog :refer [modal-container]]
-   [frontend.page.viewer :refer [page-viewer]]
-   [webly.spa.loader.page :refer [loader-page]]))
+   [frontend.page.viewer :refer [page-viewer]]))
 
 ; https://stackoverflow.com/questions/33299746/why-are-multi-methods-not-working-as-functions-for-reagent-re-frame
 ; ^{:key @current-route} [pages @current-route]
-
-(defn webly-page []
-  (let [show? (subscribe [:webly/status-show-app])]
-    (fn []
-      (if @show?
-        [page-viewer]
-        [loader-page]))))
 
 (defn webly-app []
   [:div
    [modal-container]
    [notification-container]
-   [load-css]
-   [webly-page]])
+   ;[load-css]
+   [page-viewer]])
