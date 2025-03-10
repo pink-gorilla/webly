e0164f39290374692bb280d7f065fa94db8e6bf4
awb99 <awb99@gmail.com>
2025-03-10 18:41:15 -0500

    transit.io trnsit encoding

--- demo/src/demo/page/help.cljs
+++ demo/src/demo/page/help.cljs
@@ -2,8 +2,7 @@
   (:require
    [reagent.core :as r]
    [re-frame.core :as rf]
-   [webly.spa.mode :refer [get-resource-path]]
-   [pinkgorilla.repl.cljs.http :refer [get-json]]))
+   [webly.spa.mode :refer [get-resource-path]]))
 
 (defn link-fn [fun text]
   [:a.bg-blue-300.cursor-pointer.hover:bg-red-700.m-1
@@ -16,26 +15,12 @@
   [:a.bg-blue-300.cursor-pointer.hover:bg-red-700.m-1
    {:href href} text])
 
-(def data-iss (r/atom {:data nil}))
-(def data-config (r/atom {:data nil}))
-(def firstt (r/atom true))
-
 (defn help-page [_route]
-  (let [url-moon (str (get-resource-path) "demo/moon.jpg")
-        url-config (str (get-resource-path) "config.edn")]
+  (let [url-moon (str (get-resource-path) "demo/moon.jpg")]
     [:div
      [:h1 "webly help"]
-     (when @firstt
-       (reset! firstt false)
-       (get-json "http://api.open-notify.org/iss-now.json" data-iss [:data]) ;cors test
-       (get-json url-config data-config [:data])
-       nil)
      [:p [link-dispatch [:bidi/goto 'demo.page.main/main-page] "main"]]
-     [:p "url config: " url-config]
      [:h1 "help!"]
-     [:div.bg-blue-300 "iss data (cors test - there should be data coming!):" (pr-str @data-iss)]
-     [:div.bg-red-300 "config data:" (pr-str @data-config)]
-
      [:p "a moon image should show below. this is a test for webly resource handler."]
      [:p "moon url: " url-moon]
      [:img {:src url-moon}]]))
--- spa/src/webly/spa/service/config.cljs
+++ spa/src/webly/spa/service/config.cljs
@@ -3,7 +3,7 @@
    [taoensso.timbre :refer-macros [debug info infof error]]
    [promesa.core :as p]
    [ajax.core] ; https://github.com/JulianBirch/cljs-ajax
-   [modular.encoding.transit :refer [decode]]
+   [transit.io :refer [read-opts]]
    [modular.encoding.edn :refer [read-edn]]
    [webly.spa.mode :refer [get-resource-path get-mode]]))
 
@@ -36,12 +36,16 @@
   (let [url "/api/config"
         _  (infof "loading api config from url: %s"  url)
         p-get (GET url {:timeout         10000                        ;; optional see API docs
-                        :response-format (ajax.core/transit-response-format :json decode) ;; IMPORTANT!: You must provide this.
+                        :response-format (ajax.core/transit-response-format :json (read-opts)) ;; IMPORTANT!: You must provide this.
                         })]
-    (p/then p-get
-            (fn [config]
-              (-> config
-                  (assoc :static? false))))))
+    (-> p-get
+        (p/then
+         (fn [config]
+           (-> config
+               (assoc :static? false))))
+        (p/catch
+         (fn [err]
+           (error "could not load config from " url " error: " err))))))
 
 (defn get-config []
   (let [static? (= (get-mode) :static)]
