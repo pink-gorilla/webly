d57d412871abb3fabbd20f97f6f3c16ea41934ff
awb99 <awb99@gmail.com>
2025-04-05 14:31:16 -0500

    cljs dummy test added

---
+++ demo/test/webly/dummy_test.cljs
@@ -0,0 +1,8 @@
+(ns webly.dummy-test
+  (:require 
+   [cljs.test :refer [deftest is testing run-tests]]))
+
+(deftest add-test
+  (testing "Basic addition"
+    (is (= 4 (+ 2 2)))
+    (is (= 0 (+ -1 1)))))
